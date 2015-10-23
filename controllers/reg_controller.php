95ccee482cb069af81731439e2691af5e0c29533
tyzzzon <tyzzon@yandex.ru>
2015-10-23 14:15:36 +0600

    git, mail and autoload

---
+++ controllers/reg_controller.php
@@ -0,0 +1,8 @@
+<?php
+class Reg_Controller
+{
+    public function registration($user_name, $user_surname, $user_login, $user_password, $user_email)
+    {
+
+    }
+}
--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 /*ini_set('display errors', 1);
 require_once 'bootstrap.php';*/
 //$db = new PDO('mysql:host=linkrepository;dbname=linkrepository','root','111111');
-require_once "models/user_model.php";
-require_once "models/link_model.php";
-require_once "models/temporary_link_model.php";
+require_once "autoload.php";
+//require_once "models/user_model.php";
+//require_once "models/link_model.php";
+//require_once "models/temporary_link_model.php";
 //$Vasya = new User_Model("Vasya", "Petrov", "VP", "email@email.email", "111");
 //$Petya = new User_Model("Petya", "Vasilyev", "PV", "em@em.em", "222");
 //$Vasya->lets_see();
@@ -21,7 +22,7 @@ require_once "models/temporary_link_model.php";
 //$poson->lets_see();
 //$poson->edit_user_name("Petr","PV");
 //echo $poson->user_id;
-$linky = new Temporary_Link_Model();
+//$linky = new Temporary_Link_Model();
 //$linky->create("thelinker", "linker.ru", "it is a linker than the link", 0, date("Y-m-d H:i"), 4);
 //$linky->delete_link("link.ru", 4);
 /*$linky->edit_link("thelinker", "linker.com", "it is a linker than the link", 1, 1);*/
@@ -32,4 +33,3 @@ $linky = new Temporary_Link_Model();
 //$linky->check_link("erjh56kjdf", "email@email.email");
 //$linky->send_temporary_link();
 //echo $linky->temporary_link_id."<br>".$linky->user_id."<br>".$linky->temporary_link_hash."<br>".$linky->temporary_link_born_time."<br>";
-//wtf
