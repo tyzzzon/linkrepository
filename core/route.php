f30e0766c290c810c421cb8e9cb9cc523cd970d3
tyzzzon <tyzzon@yandex.ru>
2015-10-28 16:03:23 +0600

    the true routing but still need true controlling

--- core/route.php
+++ core/route.php
@@ -14,12 +14,9 @@ class Route
         //controller and default action
         $controller_name = 'Main';
         $action_name = 'index';
-        $view_name = 'Main';
 
         $routes = ($strpos=strpos($_SERVER['REQUEST_URI'],"?"))!==false?substr($_SERVER['REQUEST_URI'],0,$strpos):$_SERVER['REQUEST_URI'];
-        //var_dump($routes);
         $routes = explode('/', $routes);
-        //var_dump($routes);
         //getting the name of the controller
         if ( !empty($routes[1]))
         {
@@ -32,15 +29,10 @@ class Route
             $action_name = $routes[2];
         }
         //adding postfixes
-        $model_name = $controller_name.'_Model';
         $controller_name = $controller_name.'_Controller';
         $action_name = $action_name.'_action';
 
         //file of model-class
-        $model_file = strtolower($model_name).'.php';
-        $model_path = "models/".$model_file;
-        if(file_exists($model_path))
-        {
             //file of controller-class
             $controller_file = strtolower($controller_name).'.php';
             $controller_path = "controllers/".$controller_file;
@@ -48,8 +40,6 @@ class Route
             {
                 //making a controller
                 $controller = new $controller_name;
-                //var_dump($action_name);
-//              var_dump($controller);
                 if (method_exists($controller, $action_name)) {
                     //calling an action of controller
                     $controller->$action_name();
@@ -63,22 +53,5 @@ class Route
             {
                 Route::ErrorPage404();
             }
-        }
-        else
-        {
-            if ( !empty($routes[1]))
-            {
-                $view_name = $routes[1];
-            }
-            $view_name = $view_name."_view";
-            if (file_exists("views/".$view_name.".php"))
-            {
-                include "views/".$view_name.".php";
-            }
-            else
-            {
-                Route::ErrorPage404();
-            }
-        }
     }
 }
